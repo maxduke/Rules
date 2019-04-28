d352a12d10c7c6e4639fd026861fc9c7ce857dc6
maxduke <maxduke@gmail.com>
2019-04-28 23:25:58 +0800

    Create WeRead.js

---
+++ Surge/Script/WeRead.js
@@ -0,0 +1,10 @@
+var result = body
+
+let path = '/pay/memberCardSummary';
+
+if (url.indexOf(path) != -1) {
+    var jsbody = JSON.parse(body);
+    jsbody.remainTime = 86313600;
+    result = JSON.stringify(jsbody);
+}
+result;
