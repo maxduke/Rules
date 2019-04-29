61fd15d60fc9505be21f8ca1f77dd7fd9fea4a42
maxduke <maxduke@gmail.com>
2019-04-29 13:55:52 +0800

    Update WeRead.js

--- Surge/Script/WeRead.js
+++ Surge/Script/WeRead.js
@@ -12,5 +12,5 @@ result;
 /******************************************************
  *来源：https://github.com/Choler/Surge/blob/master/Music.js
  *http-response ^https?://i\.weread\.qq\.com/pay/memberCard script-path= https://github.com/Choler/Surge/raw/master/WeRead.js
- *hostname = i.weiread.qq.com
+ *hostname = i.weread.qq.com
  *****************************************************/
