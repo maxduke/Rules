dd2544c6d5a6b170c3d71ef69cceea97d880985a
maxduke <maxduke@gmail.com>
2019-05-13 13:21:51 +0800

    Update and rename Music.js to KWPlayer.js

---
+++ Surge/Script/KWPlayer.js
@@ -0,0 +1,8 @@
+var obj = JSON.parse(body);
+obj['data'] = JSON.parse('{"time":"1514736000000","vipExpire":1640966400000,"biedSong":"0","isYearUser":2,"vipmExpire":1640966400000,"biedAlbum":"0","vipOverSeasExpire":1640966400000,"vip3Expire":1640966400000,"vipLuxuryExpire":1640966400000,"isNewUser":2}');
+JSON.stringify(obj);
+
+/* ************************************************************
+ * 来源：https://github.com/Choler/Surge/blob/master/Music.js
+ * http-response ^http://vip1\.kuwo\.cn/vip/ script-path=https://raw.githubusercontent.com/Choler/Surge/master/KWPlayer.js
+ *************************************************************/
