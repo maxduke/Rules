8a4979d635f6b3f076c7af1ca6948ba3f882c462
maxduke <maxduke@gmail.com>
2019-06-26 15:25:53 +0800

    Update wb_ad.js

--- Surge/Script/wb_ad.js
+++ Surge/Script/wb_ad.js
@@ -1,8 +1,3 @@
-/*
- * @script: https://raw.githubusercontent.com/yichahucha/surge/master/wb_ad.js
- * @regular: ^https?:\/\/(api|mapi)\.weibo\.(cn|com)\/2(\/groups\/timeline|\/statuses\/unread|\/statuses\/extend|\/comments\/build_comments|\/photo\/recommend_list|\/stories\/video_stream|\/statuses\/positives\/get|\/stories\/home_list|\/profile\/statuses|\/statuses\/friends\/timeline)
- */
-
 const path1 = "/groups/timeline";
 const path2 = "/statuses/unread";
 const path3 = "/statuses/extend";
@@ -13,43 +8,34 @@ const path7 = "/statuses/positives/get";
 const path8 = "/stories/home_list";
 const path9 = "/profile/statuses";
 const path10 = "/statuses/friends/timeline";
+const path11 = "/service/picfeed";
 
-var result = body;
+const url = $request.url;
+var body = $response.body;
 function filter_timeline() {
     let obj = JSON.parse(body);
     let statuses = obj.statuses;
     if (statuses && statuses.length > 0) {
-        let ad = obj.ad;
-        if (ad && ad.length > 0) {
-            for (let i = 0; i < ad.length; i++) {
-                const element = ad[i];
-                let ad_id = element.id;
-                for (let j = 0; j < statuses.length; j++) {
-                    const element = statuses[j];
-                    let statuses_id = element.id;
-                    if (statuses_id == parseInt(ad_id)) {
-                        statuses.splice(j, 1);
-                        break;
-                    }
-                }
-            }
-        }
         let i = statuses.length;
         while (i--) {
             let element = statuses[i];
-            if (is_likerecommend(element.title)) {
-                statuses.splice(i, 1);
-            }
+            if (is_timeline_likerecommend(element.title)) statuses.splice(i, 1);
+            if(is_timeline_ad(element.promotion)) statuses.splice(i, 1);
         }
-        if (obj.num) {
-            obj.num = obj.statuses.length + ad.length;
-            obj.original_num = obj.statuses.length;
-        }
+        if (obj.advertises) obj.advertises = [];
+        if (obj.ad) obj.ad = [];
+        if (obj.num) obj.num = obj.original_num;
     }
-    if (obj.trends) {
-        delete obj.trends;
-    }
-    result = JSON.stringify(obj);
+    if (obj.trends) obj.trends = [];
+    body = JSON.stringify(obj);
+}
+
+function is_timeline_ad(promotion) {
+    return (promotion && promotion.type && promotion.type == "ad") ? true : false;
+}
+
+function is_timeline_likerecommend(title) {
+    return (title && title.type && title.type == "likerecommend") ? true : false;
 }
 
 function filter_comments(datas) {
@@ -58,20 +44,10 @@ function filter_comments(datas) {
         while (i--) {
             const element = datas[i];
             let type = element.type;
-            if (type == 5 || type == 1 || type == 6) {
-                datas.splice(i, 1);
-            }
+            if (type == 5 || type == 1 || type == 6) datas.splice(i, 1);
         }
     }
     return datas;
-}
-
-function is_likerecommend(title) {
-    if (title && title.type && title.type == "likerecommend") {
-        return true;
-    } else {
-        return false;
-    }
 }
 
 if (url.indexOf(path1) != -1) {
@@ -82,16 +58,10 @@ if (url.indexOf(path2) != -1) {
     filter_timeline();
 }
 
-if (url.indexOf(path10) != -1) {
-    filter_timeline();
-}
-
 if (url.indexOf(path3) != -1) {
     let obj = JSON.parse(body);
-    if (obj.trend) {
-        delete obj.trend;
-    }
-    result = JSON.stringify(obj);
+    if (obj.trend) delete obj.trend;
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path4) != -1) {
@@ -102,19 +72,17 @@ if (url.indexOf(path4) != -1) {
             obj.max_id = obj.top_hot_structs.call_back_struct.max_id;
             delete obj.top_hot_structs;
         }
-        if (obj.datas) {
-            obj.datas = filter_comments(obj.datas);
-        }
+        if (obj.datas) obj.datas = filter_comments(obj.datas);
     } else {
         obj.datas = [];
     }
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path5) != -1) {
     let obj = JSON.parse(body);
     obj.data = {};
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path6) != -1) {
@@ -125,24 +93,22 @@ if (url.indexOf(path6) != -1) {
         while (i--) {
             const element = segments[i];
             let is_ad = element.is_ad;
-            if (typeof is_ad != "undefined" && is_ad == true) {
-                segments.splice(i, 1);
-            }
+            if (typeof is_ad != "undefined" && is_ad == true) segments.splice(i, 1);
         }
     }
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path7) != -1) {
     let obj = JSON.parse(body);
     obj.datas = [];
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path8) != -1) {
     let obj = JSON.parse(body);
     obj.story_list = [];
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
 
 if (url.indexOf(path9) != -1) {
@@ -153,12 +119,19 @@ if (url.indexOf(path9) != -1) {
         while (i--) {
             let element = cards[i];
             let card_group = element.card_group;
-            if (card_group && card_group.length > 0) {
-                cards.splice(i, 1);
-            }
+            if (card_group && card_group.length > 0) cards.splice(i, 1);
         }
     }
-    result = JSON.stringify(obj);
+    body = JSON.stringify(obj);
 }
-console.log('wb:201905151658');
-result;
+
+if (url.indexOf(path10) != -1) {
+    filter_timeline();
+}
+
+if (url.indexOf(path11) != -1) {
+    let obj = JSON.parse(body);
+    obj.data = [];
+    body = JSON.stringify(obj);
+}
+$done({body});
