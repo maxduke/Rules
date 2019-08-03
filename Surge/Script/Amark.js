e760cd6f0826048508610472bb9b62a16b09db27
maxduke <maxduke@gmail.com>
2019-08-03 10:08:45 +0800

    Create Amark.js

---
+++ Surge/Script/Amark.js
@@ -0,0 +1,9 @@
+if ($request.url.indexOf('watermark') > 0) {	
+  $done({url: $request.url.replace(/&watermark=\d/, "")});	
+} else {	
+  $done();	
+}	
+if ($request.url.indexOf('vide.f7') > 0) {	
+  $done({url: $request.url.replace(/vide\.f7/, "vide.f")});	
+} else {	
+  $done();
